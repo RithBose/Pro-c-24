3cf76ac3c0d116344e787fbf8aa36d6ab6762883
RithBose <72760072+RithBose@users.noreply.github.com>
2020-11-18 00:42:54 +0530

    Add files via upload

---
+++ BottomDustbin.js
@@ -0,0 +1,23 @@
+class BD{
+constructor(x,y){
+this.x=x
+this.y=y
+var option={isStatic:true}
+this.body= Bodies.rectangle(this.x,this.y,300,45,option)
+World.add(world,this.body)
+}
+display(){
+var pos= this.body.position
+push()
+translate(pos.x,pos.y)
+rectMode(CENTER)
+strokeWeight(4)
+stroke("yellow")
+fill("black")
+rect(0,0,200,20)
+pop();
+}
+
+
+
+}
